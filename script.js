9cbd1b849269b5ed2a6987ed790a9d872f04591e
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-11-14 14:21:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function loadImage(url){
+	return new Promise((resolve,reject)=>{
+		let img=new Image();
+		img.onload=()=>resolve(img);
+		img.onerror=()=>reject(new Error(`Failed to load images's URL ${url}`));
+		img.src=url;
+	})
+}
+btn.addEventListener("click",()=>{
+	let promises=Images.map(image=>loadImage(image.url));
+	Promise.all(promises)
+	.then(imgs=>{
+		imgs.forEach(img=>output.appendChild(img));
+	})
+	.catch(error=>console.log(error));
+})
